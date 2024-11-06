52aa8af9db917a7a5e0d53fbe8acc2bd84746374
CD8576 <148923376+CD8576@users.noreply.github.com>
2024-11-05 18:46:33 -0600

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -11,6 +11,12 @@ import { defineConfig } from "astro/config";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 // https://astro.build/config
 export default defineConfig({
+  
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
+
   output: "hybrid",
   adapter: node({
     mode: "middleware",
